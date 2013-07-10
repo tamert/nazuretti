b330b023877f9e3fa055b68d35540cdbe4b0ea39
Umur Gedik <umurgdk@gmail.com>
2013-07-10 21:13:50 +0300

    mailer syntax error fixed

--- mailer.py
+++ mailer.py
@@ -5,7 +5,7 @@ from flask.ext.mail import Mail, Message
 
 app = flask.current_app
 
-if os.environ['LAZUTTI'] == 'PRODUCTION'
+if os.environ['LAZUTTI'] == 'PRODUCTION':
   app.config['MAIL_SERVER'] = 'smtp.webfaction.com'
   app.config['MAIL_USE_SSL'] = True
   app.config['MAIL_USERNAME'] = 'lazutti'
