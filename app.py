039831338a717710f1403fbf27ea5bd339ab54cf
Umur Gedik <umurgdk@gmail.com>
2013-07-10 22:07:41 +0300

    Upload folder now points absolute path

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 import flask
 
 from flask import render_template, request, session
 from flask.ext.mail import Mail
 
 app = flask.Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './static/uploads/'
+app.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.realpath(__file__)) + '/uploads/'
 app.config['SECRET_KEY']    = 'dsfhadkjfgnfavkrniu4yi2y348734'
 app.debug = True
 
